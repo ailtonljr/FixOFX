13b1edfee9ee85f59e892d3eb8b1ad63b5d2c193
Ailton Luiz Dias Siqueira Junior <ailtonljr@gmail.com>
2017-09-30 00:26:14 -0300

    Initial Commit

---
+++ src/main/java/br/eng/ailton/FixOFXFiles/MainWindow.java
@@ -0,0 +1,245 @@
+package br.eng.ailton.FixOFXFiles;
+
+import java.awt.EventQueue;
+import java.io.File;import java.io.FilenameFilter;
+
+import javax.swing.JFrame;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.layout.FormSpecs;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class MainWindow {
+
+	private JFrame frmOfxFix;
+	private JTextField textFile;
+	private JTextField textLimite;
+
+	private JFileChooser ofxFileChooser = new JFileChooser();
+	
+	private JTextField textDir;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainWindow window = new MainWindow();
+					window.frmOfxFix.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmOfxFix = new JFrame();
+		frmOfxFix.setTitle("OFX Fix");
+		frmOfxFix.setBounds(100, 100, 602, 275);
+		frmOfxFix.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmOfxFix.getContentPane().setLayout(null);
+		
+		JLabel lblFilename = new JLabel("Arquivo");
+		lblFilename.setBounds(10, 38, 56, 16);
+		frmOfxFix.getContentPane().add(lblFilename);
+		
+		textFile = new JTextField();
+		textFile.setBounds(78, 33, 445, 26);
+		frmOfxFix.getContentPane().add(textFile);
+		textFile.setColumns(10);
+		
+		JButton btnAbrirArquivo = new JButton("...");
+		btnAbrirArquivo.setBounds(521, 33, 45, 29);
+		
+		btnAbrirArquivo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				
+				FileFilter filter = new FileNameExtensionFilter("Arquivo OFX","ofx");
+			    
+				ofxFileChooser.setFileFilter(filter);
+				
+				int result = ofxFileChooser.showOpenDialog(frmOfxFix);
+				
+				if(result == JFileChooser.APPROVE_OPTION)
+				{
+					textFile.setText(ofxFileChooser.getSelectedFile().getAbsolutePath());
+					ofxFileChooser.setCurrentDirectory(ofxFileChooser.getSelectedFile().getParentFile());
+				}
+				
+			    
+				
+			}
+		});
+		
+		frmOfxFix.getContentPane().add(btnAbrirArquivo);
+		
+		JButton btnCorrigirCartoDe = new JButton("Corrigir Cartão de Crédito");
+		btnCorrigirCartoDe.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				String ofxFile = textFile.getText();
+		        
+		        OFXEditor editor = new OFXEditor();
+		        
+		        editor.setOfxFile(new File(ofxFile));
+		        
+		        editor.FixVisa();
+		        JOptionPane.showMessageDialog(frmOfxFix, "Arquivo corrigido.");
+				
+			}
+		});
+		btnCorrigirCartoDe.setBounds(4, 66, 206, 29);
+		frmOfxFix.getContentPane().add(btnCorrigirCartoDe);
+		
+		JLabel lblLimite = new JLabel("Limite");
+		lblLimite.setBounds(209, 71, 39, 16);
+		frmOfxFix.getContentPane().add(lblLimite);
+		
+		textLimite = new JTextField();
+		textLimite.setText("5000");
+		textLimite.setBounds(260, 66, 88, 26);
+		frmOfxFix.getContentPane().add(textLimite);
+		textLimite.setColumns(10);
+		
+		JButton btnCorrigirSaldoDa = new JButton("Corrigir saldo da Caixa");
+		btnCorrigirSaldoDa.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				String ofxFile = textFile.getText();
+				double limite = Double.parseDouble( textLimite.getText());
+		        
+				
+		        OFXEditor editor = new OFXEditor();
+		        
+		        editor.setOfxFile(new File(ofxFile));
+		        
+		        editor.FixBalanceOnCaixa(limite);
+		        
+		        JOptionPane.showMessageDialog(frmOfxFix, "Arquivo corrigido.");
+			}
+		});
+		btnCorrigirSaldoDa.setBounds(360, 66, 206, 29);
+		frmOfxFix.getContentPane().add(btnCorrigirSaldoDa);
+		
+		JLabel lblDiretrio = new JLabel("Diretório");
+		lblDiretrio.setBounds(14, 114, 61, 16);
+		frmOfxFix.getContentPane().add(lblDiretrio);
+		
+		textDir = new JTextField();
+		textDir.setBounds(78, 109, 445, 26);
+		frmOfxFix.getContentPane().add(textDir);
+		textDir.setColumns(10);
+		
+		JButton button = new JButton("...");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				FileFilter filter = new FileNameExtensionFilter("Arquivo OFX","ofx");
+			    
+				ofxFileChooser.setFileFilter(filter);
+				
+				int result = ofxFileChooser.showOpenDialog(frmOfxFix);
+				
+				if(result == JFileChooser.APPROVE_OPTION)
+				{
+					textDir.setText(ofxFileChooser.getSelectedFile().getParent());
+					ofxFileChooser.setCurrentDirectory(ofxFileChooser.getSelectedFile().getParentFile());
+				}
+			}
+		});
+		button.setBounds(521, 107, 45, 29);
+		frmOfxFix.getContentPane().add(button);
+		
+		JButton btnCorrigirCartoDe_1 = new JButton("Corrigir Cartão de Crédito");
+		btnCorrigirCartoDe_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				File ofxFilesDir = new File(textDir.getText());
+				FilenameFilter filter = new FilenameFilter() {
+				    public boolean accept(File dir, String name) {
+				        return name.toLowerCase().endsWith(".ofx") && !name.startsWith("Fixed");
+				    }
+				};
+				
+				File [] ofxFilesList = ofxFilesDir.listFiles(filter);
+				
+				for(File ofxFile : ofxFilesList)
+				{
+					
+					OFXEditor editor = new OFXEditor();
+			        
+			        editor.setOfxFile(ofxFile);
+			        
+			        editor.FixVisa();
+					
+				}
+				
+				JOptionPane.showMessageDialog(frmOfxFix, "Arquivos corrigidos.");
+				
+				
+			}
+		});
+		btnCorrigirCartoDe_1.setBounds(6, 142, 204, 29);
+		frmOfxFix.getContentPane().add(btnCorrigirCartoDe_1);
+		
+		JButton btnNewButton = new JButton("Corrigir saldo da Caixa");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				File ofxFilesDir = new File(textDir.getText());
+				FilenameFilter filter = new FilenameFilter() {
+				    public boolean accept(File dir, String name) {
+				        return name.toLowerCase().endsWith(".ofx") && !name.startsWith("Fixed");
+				    }
+				};
+				
+				File [] ofxFilesList = ofxFilesDir.listFiles(filter);
+				
+				for(File ofxFile : ofxFilesList)
+				{
+					
+					OFXEditor editor = new OFXEditor();
+			        
+			        editor.setOfxFile(ofxFile);
+			        
+			        double limite = Double.parseDouble(textLimite.getText());
+			        
+			        
+			        editor.FixBalanceOnCaixa(limite);
+					
+				}
+				
+				JOptionPane.showMessageDialog(frmOfxFix, "Arquivos corrigidos.");
+				
+			}
+		});
+		btnNewButton.setBounds(360, 142, 200, 29);
+		frmOfxFix.getContentPane().add(btnNewButton);
+	}
+}
---
+++ src/main/java/br/eng/ailton/FixOFXFiles/OFXEditor.java
@@ -0,0 +1,245 @@
+package br.eng.ailton.FixOFXFiles;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.webcohesion.ofx4j.domain.data.MessageSetType;
+import com.webcohesion.ofx4j.domain.data.ResponseEnvelope;
+import com.webcohesion.ofx4j.domain.data.ResponseMessageSet;
+import com.webcohesion.ofx4j.domain.data.banking.BankStatementResponseTransaction;
+import com.webcohesion.ofx4j.domain.data.banking.BankingResponseMessageSet;
+import com.webcohesion.ofx4j.domain.data.common.Transaction;
+import com.webcohesion.ofx4j.domain.data.creditcard.CreditCardResponseMessageSet;
+import com.webcohesion.ofx4j.domain.data.creditcard.CreditCardStatementResponseTransaction;
+import com.webcohesion.ofx4j.domain.data.signon.SignonResponse;
+import com.webcohesion.ofx4j.io.AggregateMarshaller;
+import com.webcohesion.ofx4j.io.AggregateUnmarshaller;
+import com.webcohesion.ofx4j.io.OFXParseException;
+import com.webcohesion.ofx4j.io.v1.OFXV1Writer;
+import com.webcohesion.ofx4j.io.v2.OFXV2Writer;
+
+
+/**
+ * @author Ailton Luiz Dias Siqueira Junior
+ *
+ */
+public class OFXEditor {
+	
+	
+	
+	private File ofxFile;
+
+	public File getOfxFile() {
+		return ofxFile;
+	}
+
+	public void setOfxFile(File ofxFile) {
+		this.ofxFile = ofxFile;
+	}
+	
+	private ResponseEnvelope responseEnvelope;
+	
+	private ResponseMessageSet loadOfxFile(MessageSetType type) throws FileNotFoundException, IOException
+	{
+		
+		AggregateUnmarshaller<ResponseEnvelope> unmarshaller = new AggregateUnmarshaller<ResponseEnvelope>(
+				ResponseEnvelope.class);
+
+		
+		
+		ResponseMessageSet responseMessageSet = null;
+		
+		try {
+			responseEnvelope = unmarshaller.unmarshal(new FileInputStream(ofxFile));
+			
+			SignonResponse sr = responseEnvelope.getSignonResponse();
+			
+			responseMessageSet = responseEnvelope.getMessageSet(type);
+			
+		} catch (OFXParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
+		return responseMessageSet;
+	}
+	
+	
+	private void saveOfxFile(ResponseMessageSet messageSet) throws IOException {
+		
+		//StringWriter marshalled = new StringWriter();
+		File fileToSave = new File(ofxFile.getParent() + File.separator + "Fixed" + ofxFile.getName());
+		
+		FileWriter marshalled = new FileWriter(fileToSave);
+		
+		OFXV1Writer writer = new OFXV1Writer(marshalled);
+		
+		AggregateMarshaller marsheller = new AggregateMarshaller();
+		marsheller.marshal(responseEnvelope, writer);
+		
+		writer.close();
+		
+		
+		
+		
+
+		/*try (PrintWriter out = new PrintWriter(fileToSave.getAbsolutePath())) {
+			out.println(marshalled.toString());
+			
+		}*/
+		
+	}
+	
+	public void FixBalanceOnCaixa(double creditLimit) {
+		
+		
+		try {
+			
+			ResponseMessageSet message = loadOfxFile(MessageSetType.banking);
+			
+			
+			//Find balace and subtract the credit limit
+			if (message != null) {
+				List<BankStatementResponseTransaction> bank = ((BankingResponseMessageSet) message)
+						.getStatementResponses();
+				for (BankStatementResponseTransaction b : bank) {
+					System.out.println("cc: " + b.getMessage().getAccount().getAccountNumber());
+					// System.out.println("ag: " + b.getMessage().getAccount().getBranchId());
+					double balance = b.getMessage().getLedgerBalance().getAmount();
+					System.out.println("balanço final: " + b.getMessage().getLedgerBalance().getAmount());
+					
+					
+					
+					double newBalance = balance - creditLimit;
+					BigDecimal modelVal = new BigDecimal(newBalance);
+					BigDecimal displayVal = modelVal.setScale(2, RoundingMode.HALF_EVEN);
+					newBalance = displayVal.doubleValue();
+					b.getMessage().getLedgerBalance().setAmount(newBalance);
+					
+					System.out.println("balanço final Corrigido: " + b.getMessage().getLedgerBalance().getAmount());
+					
+					
+					
+					System.out.println("dataDoArquivo: " + b.getMessage().getLedgerBalance().getAsOfDate());
+					List<Transaction> list = b.getMessage().getTransactionList().getTransactions();
+					System.out.println("TRANSAÇÕES\n");
+					for (Transaction transaction : list) {
+						System.out.println("tipo: " + transaction.getTransactionType().name());
+						System.out.println("id: " + transaction.getId());
+						System.out.println("data: " + transaction.getDatePosted());
+						System.out.println("valor: " + transaction.getAmount());
+						System.out.println("descricao: " + transaction.getMemo());
+						System.out.println("");
+					}
+				}
+			}
+			
+			
+			
+			saveOfxFile(message);
+			
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+	
+	private final Pattern visaInstallmentPattern = Pattern.compile("PARC \\d+\\/\\d+");
+	
+	public void FixVisa() {
+		
+		
+		try {
+			
+			ResponseMessageSet message = loadOfxFile(MessageSetType.creditcard);
+			
+			
+			//Find balace and subtract the credit limit
+			if (message != null) {
+				List<CreditCardStatementResponseTransaction> bank = ((CreditCardResponseMessageSet) message)
+						.getStatementResponses();
+				for (CreditCardStatementResponseTransaction b : bank) {
+					System.out.println("cc: " + b.getMessage().getAccount().getAccountNumber());
+					// System.out.println("ag: " + b.getMessage().getAccount().getBranchId());
+			
+					System.out.println("balanço final: " + b.getMessage().getLedgerBalance().getAmount());
+
+					System.out.println("dataDoArquivo: " + b.getMessage().getLedgerBalance().getAsOfDate());
+					List<Transaction> list = b.getMessage().getTransactionList().getTransactions();
+					System.out.println("TRANSAÇÕES\n");
+					for (Transaction transaction : list) {
+						System.out.println("tipo: " + transaction.getTransactionType().name());
+						System.out.println("id: " + transaction.getId());
+						System.out.println("data: " + transaction.getDatePosted());
+						
+						Matcher memoMatcher = visaInstallmentPattern.matcher(transaction.getMemo());
+						
+						
+						
+						if(memoMatcher.find())
+						{
+							
+							
+							int installmentNumber = Integer.parseInt( memoMatcher.group().substring(5, 7) );
+							
+							Date date = transaction.getDatePosted();
+							
+							Calendar cal = Calendar.getInstance();
+							cal.setTime(date); // your date (java.util.Date)
+							cal.add(Calendar.MONTH, installmentNumber - 1); // You can -/+ x months here to go back in history or move forward.
+							date = cal.getTime(); // New date
+							transaction.setDatePosted(date);
+							System.out.println("data modificada: " + transaction.getDatePosted());
+							
+						}
+						
+						
+						System.out.println("valor: " + transaction.getAmount());
+						System.out.println("descricao: " + transaction.getMemo());
+						System.out.println("");
+					}
+				}
+			}
+			
+			
+			
+			saveOfxFile(message);
+			
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+	
+
+}
